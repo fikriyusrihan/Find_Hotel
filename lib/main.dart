3ddc729efa38c6655eda3018464fad18120a7c51
Fikri Yusrihan <yusrihanfikri@gmail.com>
2021-05-22 14:13:43 +0700

    Add Icon for Android✨

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Airbnb Hotel and Travel',
         theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Work Sans'),
         home: MainScreen());
